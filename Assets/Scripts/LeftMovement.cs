b78203f71b041d168f1b222743e7747bbfa90985
gturksever <gsgturk@hotmail.com>
2023-05-29 20:15:33 +0300

    Added ObstacleSpawner
    Added ObstacleSpawner

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -7,11 +7,21 @@ public class LeftMovement : MonoBehaviour
     public float speed;
     BoxCollider2D box;
     float groundWidth;
+    float obstacleWidth;
     
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWidth = box.size.x;
+        if (gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidth = box.size.x;
+        }
+
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
+        
         
     }
 
@@ -20,10 +30,15 @@ public class LeftMovement : MonoBehaviour
     {
         transform.position = new Vector2(transform.position.x -speed *Time.deltaTime,transform.position.y);
 
-        if(transform.position.x <= -groundWidth)
+        if (gameObject.CompareTag("Ground"))
         {
-            transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
+            if (transform.position.x <= -groundWidth)
+            {
+                transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
 
+            }
         }
+        
+        
     }
 }
