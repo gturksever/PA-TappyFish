c3242f6f1bd10d86ca4618e9010b54e79b75c6d0
gturksever <gsgturk@hotmail.com>
2023-05-31 21:33:53 +0300

    Added PlayerPrefs and GameOver Panel
    Added PlayerPrefs and GameOver Panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static Vector2 bottomLeft;
     public static bool gameOver;
+    public GameObject gameOverPanel;
     private void Awake()
     {
         bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
@@ -14,12 +16,19 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        
         gameOver = false;
     }
 
     public  void GameOver()
     {
         gameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     
     void Update()
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     int score;
+    int highScore;
     Text scoreText;
+    public Text panelScore;
+    public Text panelHighScore;
 
     
     void Start()
@@ -13,12 +16,23 @@ public class Score : MonoBehaviour
         score = 0;
         scoreText = GetComponent<Text>();
         scoreText.text = score.ToString();
+        panelScore.text = score.ToString();
+        highScore = PlayerPrefs.GetInt("highscore");
+        panelHighScore.text = highScore.ToString();
     }
 
     public void Scored()
     {
         score++;
         scoreText.text = score.ToString();
+        panelScore.text = score.ToString();
+
+        if(score > highScore)
+        {
+            highScore = score;
+            panelHighScore.text = highScore.ToString();
+            PlayerPrefs.SetInt("highscore",highScore);
+        }
     }
    
     void Update()
